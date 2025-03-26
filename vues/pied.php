e50a9490a18b9b59c923c28c4e42c81fc7be781e
RenouaFF <168977276+RenouaFF@users.noreply.github.com>
2025-03-27 00:15:20 +0100

    [PHASE 2] Create pied.php

---
+++ vues/pied.php
@@ -0,0 +1,36 @@
+   </div>
+    <footer>
+        <div class="container">
+            <div class="footer-content">
+                <div class="footer-column">
+                    <h4>Fairy Escape</h4>
+                    <p> Plongez dans des séjours enchanteurs inspirés de vos légendes préférées et vivez une expérience inoubliable, entre rêve et réalité.</p>
+                </div>
+                
+                <div class="footer-column">
+                    <h4>Liens rapides</h4>
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Voyages</a></li>
+                        <li><a href="#">Qui-sommes nous</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+                
+                <div class="footer-column">
+                    <h4>Contact</h4>
+                    <ul>
+                        <li>Email:  info@fairyescapes.com</li>
+                        <li>Téléphone: (+33) 456-7890</li>
+                        <li>Adresse: 123 Street, PAris</li>
+                    </ul>
+                </div>
+            </div>
+            
+            <div class="copyright">
+                <p>&copy; 2025 Fairy Escapes. Tous droits réservés.</p>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
