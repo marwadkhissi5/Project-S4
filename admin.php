c67540c3ab906e07201de2f6541c2d7d394d88ee
RenouaFF <168977276+RenouaFF@users.noreply.github.com>
2025-05-11 20:33:38 +0200

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,76 +1,79 @@
-<?php
+<?php 
 session_start();
+include 'includes/fcts_donnees.php';
+include 'vues/entete.php';
+
+$stylesheets = [];
+$javascripts = [];
 
 if (!isset($_SESSION['utilisateur']) || $_SESSION['utilisateur']['role'] !== 'admin') {
-    header("Location: connexion.php");
-}
+    header("Location: index.php");
+} ?>
 
-$fichier = 'donnees/bd.json';
-$donnees = file_get_contents($fichier);
-$decoded_data = json_decode($donnees, true);
-$utilisateurs=$decoded_data['utilisateurs'];
+<?php include 'vues/recherche.php' ;?>
 
-$utilisateurs_par_page = 2;
-$utilisateurs_tot = count($utilisateurs);
-$pages_tot = ceil($utilisateurs_tot / $utilisateurs_par_page);
+<?php 
+    $utilisateurs=$bd["utilisateurs"];
+    $nombre_tot_page=ceil(count($utilisateurs)/$nb_par_page);
+    $page=1;
 
-$page=1;
-if (isset($_GET['page']))
-    $page=$_GET['page'];
+    if (isset($_GET['page']))
+        $page=$_GET['page'];
 
-
-$debutp = ($page - 1) * $utilisateurs_par_page;
+    $debutp=(($page-1)*$nb_par_page);
 ?>
-
-<?php $titre="Administrateur"; ?>
-<?php include "vues/entete.php" ?>
-
-<section class="bg-fairy">
-    <div class="container">
+<section>
+    <div class="conteneur">
         <h2 class="title">Gestion des Utilisateurs</h2>
-        <table border="1">
+        
+        <table class="table">
             <thead>
                 <tr>
-                    <th>Login</th>
-                    <th>Nom</th>
-                    <th>Naissance</th>
-                    <th>Adresse</th>
-                    <th>Email</th>
-                    <th>Statut</th>
-                    <th>Inscription</th>
-                    <th>Actions</th>
+                  <th>Login</th>
+                  <th>Nom</th>
+                  <th>Email</th>
+                  <th>Rôle</th>
+                  <th>Date de naissance</th>
+                  <th>Date d'inscription</th>
+                  <th>Adresse</th>
+                  <th>Actions</th>
                 </tr>
-            </thead>
+              </thead>
             <tbody>
-                <?php foreach (array_slice($utilisateurs, $debutp, $utilisateurs_par_page) as $utilisateurs): ?>
+                <?php foreach (array_slice($utilisateurs, $debutp, $nb_par_page) as $utilisateurs): ?>
                     <tr>
                         <td><?php echo $utilisateurs['login'];?></td>
                         <td><?php echo $utilisateurs['informations']['nom'];?></td>
-                        <td><?php echo $utilisateurs['informations']['naissance'];?></td>
-                        <td><?php echo $utilisateurs['informations']['adresse']?></td>
                         <td><?php echo $utilisateurs['email'];?></td>
                         <td><?php echo $utilisateurs['role'];?></td>
+                        <td><?php echo $utilisateurs['informations']['naissance'];?></td>
                         <td><?php echo $utilisateurs['dates']['inscription'];?></td>
-                        <td></td>
+                        <td><?php echo $utilisateurs['informations']['adresse']?></td>
+                        <td class="actions">
+                            <a href="modifier_utilisateur.php?id=<?php echo $utilisateurs['login'];?>" class="btn-edit">✏️</a>
+                            <button class="btn-delete">🗑</button>
+                        </td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
-    </div>
-    <div class="pagination">
-        <?php 
-            echo '<a href="admin.php?page='.($page-1).'" class="pagination-item">&laquo;</a>';
-            for ($i=1; $i <= $pages_tot; $i++) { 
-                if ($page==$i){
-                    echo '<a href="admin.php?page='.$i.'" class="pagination-item active">'.$i.'</a>';
+        <button class="btn btn-template btn-large">➕ Ajouter un utilisateur</button>
+        <div class="pagination">
+            <?php 
+                echo '<a href="admin.php?page='.($page-1).'" class="pagination-item">&laquo;</a>';
+                for ($i=1; $i <= $nombre_tot_page; $i++) { 
+                    if ($page==$i){
+                        echo '<a href="admin.php?page='.$i.'" class="pagination-item active">'.$i.'</a>';
+                    }
+                    else{
+                        echo '<a href="admin.php?page='.$i.'" class="pagination-item">'.$i.'</a>';
+                    }
                 }
-                else{
-                    echo '<a href="admin.php?page='.$i.'" class="pagination-item">'.$i.'</a>';
-                }
-            }
-            echo '<a href="admin.php?page='.($page+1).'" class="pagination-item">&raquo;</a>'
-        ?>
+                echo '<a href="admin.php?page='.($page+1).'" class="pagination-item">&raquo;</a>'
+            ?>
+        </div>
     </div>
 </section>
+    
 
-<?php include "vues/pied.php" ?>
+<?php include 'vues/pied.php' ;?>
