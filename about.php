75a8254f0051a3fb8efc31758c45f6e4011f687c
RenouaFF <168977276+RenouaFF@users.noreply.github.com>
2025-05-11 20:31:42 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,74 @@
+<?php 
+  include 'vues/entete.php';
+  $stylesheets = [];
+  $javascripts = [];
+?>
+
+<?php include 'vues/recherche.php' ;?>
+
+<section class="section-a-propos bg-clair">
+  <div class="conteneur">
+    <h1 class="titre-principal">À propos de Fairy Escapes</h1>
+
+    <h2>Notre histoire</h2>
+    <p>
+      Fairy Escapes est né de la passion du voyage et du rêve d’offrir des
+      expériences inoubliables inspirées des contes de fées. Nous avons
+      conçu des séjours uniques, basés sur les aventures de princesses
+      iconiques, pour transporter nos voyageurs dans un monde de magie et de
+      découverte.
+    </p>
+    <p>
+      Depuis sa création, Fairy Escapes a permis à des centaines de
+      voyageurs de vivre des moments enchanteurs dans des destinations de
+      rêve. Chaque itinéraire est soigneusement conçu pour allier culture,
+      nature et féerie, tout en assurant une expérience immersive et
+      authentique.
+    </p>
+
+    <h2>Notre mission</h2>
+    <p>
+      Créer des voyages immersifs et enchanteurs qui éveillent l’imaginaire,
+      tout en respectant la culture et l’authenticité des destinations.
+    </p>
+    <p>
+      Nous croyons que le voyage est une porte vers de nouvelles expériences
+      et une meilleure compréhension du monde. C'est pourquoi nous nous
+      engageons à proposer des séjours qui respectent les traditions locales
+      et favorisent les échanges avec les populations sur place.
+    </p>
+
+    <h2>Notre équipe</h2>
+    <div class="bloc-equipe">
+      <div class="membre-equipe">
+        <h4>Renoua</h4>
+        <p>Passionnée de voyages</p>
+      </div>
+
+      <div class="membre-equipe">
+        <h4>Maïssa</h4>
+        <p>Coordinatrice des séjours</p>
+      </div>
+
+      <div class="membre-equipe">
+        <h4>Marwa</h4>
+        <p>Créatrice d’expériences immersives</p>
+      </div>
+    </div>
+
+    <p style="margin-top: 30px">
+      Notre équipe est composée de passionnés de voyages et d’experts du
+      tourisme, dédiés à la création d’expériences uniques et magiques pour
+      nos voyageurs. Nous travaillons avec des guides locaux, des
+      coordinateurs expérimentés et des spécialistes du storytelling pour
+      donner vie à chaque séjour. Chez Fairy Escapes, nous croyons en
+      l'importance d'une équipe unie et passionnée. Chaque membre apporte
+      son expertise et sa créativité pour concevoir des séjours qui mêlent
+      aventure, émerveillement et immersion culturelle. Grâce à notre
+      engagement et notre amour du voyage, nous faisons en sorte que chaque
+      escapade soit inoubliable.
+    </p>
+  </div>
+</section>
+
+<?php include 'vues/pied.php' ;?>
